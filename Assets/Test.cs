e8ddf5683f3c20cb864b6300c10b473e6350a557
jiangyidu <397928901@qq.com>
2021-04-26 17:16:03 +0800

    添加工程

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Transform Cube;
+    public Transform Sphyer;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // angle += Time.deltaTime;
+        Sphyer.RotateAround(Cube.transform.position, Vector3.forward, 3);
+    }
+}
